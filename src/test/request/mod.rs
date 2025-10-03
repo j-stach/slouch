4119a755ef7114260fa8a43a0fb12662262c0638
j-stach <jordan.stachura@gmail.com>
2025-10-03 14:46:07 -0400

    moved encode test responsibility to subtypes

--- src/test/request/mod.rs
+++ src/test/request/mod.rs
@@ -2,20 +2,3 @@
 #[cfg(test)] mod cancel;
 #[cfg(test)] mod query;
 
-use crate::{ msg::OuchRequest, account_query };
-
-#[test] fn request_new_encode() {
-
-    // Macros are tested in the doc comments 
-    let request = account_query!();
-
-    let bytes = request.clone().to_bytes();
-
-    let mut should_be: Vec<u8> = vec![b'Q'];
-    should_be.extend(0u16.to_be_bytes());
-
-    assert_eq!(bytes, &should_be[0..3]);
-}
-
-// TODO: Test encodings for other messages
-
--- src/test/request/query.rs
+++ src/test/request/query.rs
@@ -1,17 +1,43 @@
 
 use crate::{ account_query, msg::TagValue };
 
-#[test] fn account_query() {
+#[test] fn new() {
 
+    // Macros are tested in the doc comments 
     let mut request = account_query!();
     let options = request.options();
     assert!(options.is_empty());
 
     request.add_option(TagValue::UserRefIndex(0u8))
-        .expect("Should probably be a good optional value");
+        .expect("Should be a good optional value");
     let options = request.options();
     assert!(!options.is_empty());
 }
 
-// TODO: Encode?
+#[test] fn encode() {
+
+    let mut request = account_query!();
+
+    let bytes = request.clone().to_bytes();
+
+    let mut should_be: Vec<u8> = vec![b'Q'];
+    should_be.extend(0u16.to_be_bytes());
+    assert_eq!(bytes, should_be);
+
+    request.add_option(TagValue::UserRefIndex(0u8))
+        .expect("Should be a good optional value");
+    let bytes = request.clone().to_bytes();
+
+    // Include the request type tag
+    let mut should_be: Vec<u8> = vec![b'Q'];
+    // Include the appendage length marker
+    should_be.extend(3u16.to_be_bytes());
+    // Include the tag value length marker
+    should_be.push(2u8);
+    // Include option tag
+    should_be.push(28u8);
+    // Include the UserRefIndex byte 
+    should_be.push(0u8);
+    assert_eq!(bytes, should_be);
+}
 
